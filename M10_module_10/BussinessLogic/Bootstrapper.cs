cfa7c3f92b1d0c6486fb0cf667fbf0c9560483bf
Mikhail Savelev <saveliev.m.v@yandex.ru>
2022-05-12 12:05:14 +0300

    df

--- M10_module_10/BussinessLogic/Bootstrapper.cs
+++ M10_module_10/BussinessLogic/Bootstrapper.cs
@@ -32,6 +32,7 @@ namespace BussinessLogic
                         default:
                             throw new OutputTypeReportException();
                     }
+
                 });
         }
     }
